8bb0bb776db1e0c9fa59a569eb87ce10e0bc41cb
Jean Vilver <jean.vilver@gmail.com>
2018-11-07 22:14:49 +0100

    Add Run-length encoding of a list in scala alternative

--- 1-working-with-list/p10_encode.scala
+++ 1-working-with-list/p10_encode.scala
@@ -5,6 +5,7 @@ object Main {
   def main(args: Array[String]) {
     val l = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e" ,"e")
     println(encode(pack(l)))
+    println(encode2(l))
   }
 
   def pack[T](lst: List[T]): List[List[T]] = {
@@ -27,5 +28,20 @@ object Main {
       case Nil => Nil
       case hd :: tail => (hd.length, hd(0)) :: encode(tail)
     }
+  }
+
+  def encode2[T](lst: List[T]): List[(Int,T)] = {
+    def aux[T](count: Int, acc: List[(Int, T)], l: List[T]): List[(Int, T)] = {
+      l match {
+        case Nil => acc
+        case hd :: Nil => (count+1, hd) :: acc
+        case a :: b :: t =>
+          if(a == b)
+            aux(count+1, acc, b :: t)
+          else
+            aux(0, (count+1, a) :: acc, b :: t)
+      }
+    }
+    aux(0, List(), lst).reverse
   }
 }
