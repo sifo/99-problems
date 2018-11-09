61ce9eb16b8fb980a3d33f1be623625f9b1d67cd
Jean Vilver <jean.vilver@gmail.com>
2018-11-09 11:38:34 +0100

    Fix Modified run-length encoding

--- 1-working-with-list/p11_decode.scala
+++ 1-working-with-list/p11_decode.scala
@@ -7,7 +7,7 @@ object Main {
 
   def main(args: Array[String]) {
     val l = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e" ,"e")
-    println(decode(encode(l)))
+    println(encode(_encode(l)))
   }
 
   def encode[T](lst: List[T]): List[(Int,T)] = {
@@ -25,14 +25,14 @@ object Main {
     aux(0, List(), lst).reverse
   }
 
-  def decode[T](l: List[(Int, T)]): List[Node[T]] = {
+  def encode[T](l: List[(Int, T)]): List[Node[T]] = {
     l match {
       case Nil => Nil
       case hd :: tail =>
         if(hd._1 == 1)
-          One(hd._2) :: decode(tail)
+          One(hd._2) :: encode(tail)
         else
-          Many(hd._1, hd._2) :: decode(tail)
+          Many(hd._1, hd._2) :: encode(tail)
     }
   }
 }
