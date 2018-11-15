796dd0915e3f9b9b8231ede4436edaf02a302fd1
Jean Vilver <jean.vilver@gmail.com>
2018-11-15 23:13:06 +0100

    Add Drop every N'th element from a list in scala

---
+++ 1-working-with-list/p16_drop.scala
@@ -0,0 +1,22 @@
+
+object Main {
+
+  def main(args: Array[String]) {
+    val l = List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
+    println(dropEveryNth(l, 3))
+  }
+
+  def dropEveryNth[T](list: List[T], n: Int): List[T] = {
+    def aux(l: List[T], c: Int, acc: List[T]): List[T] = {
+      l match {
+        case Nil => acc
+        case hd :: tail =>
+          if(c <= 1)
+            aux(tail, n, acc)
+          else
+            aux(tail, c-1, hd :: acc)
+      }
+    }
+    aux(list, n, List()).reverse
+  }
+}
