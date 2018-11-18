858bce37f5d03c19dd29e0bc5970f852662bab93
Jean Vilver <jean.vilver@gmail.com>
2018-11-18 11:11:57 +0100

    Add alternative to Rotate a list N places to the left in scala

--- 1-working-with-list/p19_rotate.scala
+++ 1-working-with-list/p19_rotate.scala
@@ -5,6 +5,8 @@ object Main {
     val l = List("a", "b", "c", "d", "e", "f", "g", "h")
     println(rotate(l, 3))
     println(rotate(l, -2))
+    println(rotate2(l, 3))
+    println(rotate2(l, -2))
   }
 
   def rotate[T](list: List[T], n: Int): List[T] = {
@@ -22,6 +24,31 @@ object Main {
       }
     } else {
       list
+    }
+  }
+
+
+  def split[T](list: List[T], n: Int): (List[T], List[T]) = {
+    def aux(l: List[T], c: Int, acc: (List[T], List[T])): (List[T], List[T]) = {
+      l match {
+        case Nil => (acc._1.reverse, List())
+        case hd :: tail =>
+          if(c == 0)
+            (acc._1.reverse, l)
+          else
+            aux(tail, c-1, (hd :: acc._1, tail))
+      }
+    }
+    aux(list, n, (List(), List()))
+  }
+
+  def rotate2[T](list: List[T], n: Int): List[T] = {
+    if(n == 0) {
+      list
+    } else {
+      val m = (n % list.length + list.length) % list.length
+      val r = split(list, m)
+      r._2 ::: r._1
     }
   }
 }
