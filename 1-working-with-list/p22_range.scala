c850616368eb49c994b2cef486dfb2f266aa3a6b
Jean Vilver <jean.vilver@gmail.com>
2018-11-24 10:06:04 +0100

    Add alternative

--- 1-working-with-list/p22_range.scala
+++ 1-working-with-list/p22_range.scala
@@ -4,6 +4,8 @@ object Main {
   def main(args: Array[String]) {
     println(range(4, 9))
     println(range(9, 4))
+    println(range2(4, 9))
+    println(range2(9, 4))
   }
 
   def range(a: Int, b: Int): List[Int] = {
@@ -13,5 +15,17 @@ object Main {
       a :: range(a-1, b)
     else
       List(a)
+  }
+
+  def range2(a: Int, b: Int): List[Int] = {
+    def aux(cur: Int, acc: List[Int]): List[Int] = {
+      if(cur < b)
+        aux(cur+1, cur :: acc)
+      else if(b < cur)
+        aux(cur-1, cur :: acc)
+      else
+        (cur :: acc).reverse
+    }
+    aux(a, List())
   }
 }
